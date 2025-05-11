03367ffb482031871383b49dcf656e50ac14f9f6
Red.prjects <red.me1aqw@gmail.com>
2025-05-11 19:02:13 +0200

    Add the add.html and the storage file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -12,9 +12,15 @@ def load_posts(filename='posts.json'):
         return []
 
 
+# Save blog posts to JSON file
+def save_posts(posts, filename='posts.json'):
+    with open(filename, 'w') as f:
+        json.dump(posts, f, indent=4)
+
+
 # Attaches the flask app to the home route.
 @app.route('/')
-def home():
+def index():
     """
     Calls the load_posts function by creating a variable and returns the index file
     and passing the posts parameter into the blog_posts argument.
@@ -22,9 +28,42 @@ def home():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+
+    # IF a post request is sent to this route, it will add a new blog post
+    # Route to add a new blog post
+    if request.method == 'POST':
+        # Get data from the form
+        title = request.form.get('title')
+        author = request.form.get('author')
+        content = request.form.get('content')
+
+        # Generate the next ID for the new post
+        blog_posts = load_posts()
+        new_id = max([post['id'] for post in blog_posts], default=0) + 1
+
+        # Create the new blog post
+        new_post = {
+            'id': new_id,
+            'author': author,
+            'title': title,
+            'content': content
+        }
+
+        # Add the new post to the list and save it
+        blog_posts.append(new_post)
+        save_posts(blog_posts)
+
+        # Redirect back to the homepage
+        return redirect(url_for('index'))
+
+    # If a GET request, render the form for adding a new post
+    # display a form for adding a new blog post.
+    return render_template('add.html')
+
+
 
 
 # Runs the flask app by calling the run method.
